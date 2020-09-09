767fdf970ea82b59cbecd47071a8c4b17a3c7a29
Jakub Znamenacek <jakub.znamenacek@siemens.com>
2020-09-09 08:50:28 +0200

    Enables CORS for all origins

--- src/main/java/com/siemens/metal_forming/ApiConfiguration.java
+++ src/main/java/com/siemens/metal_forming/ApiConfiguration.java
@@ -2,17 +2,20 @@ package com.siemens.metal_forming;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.AntPathMatcher;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
+@EnableWebMvc
 public class ApiConfiguration implements WebMvcConfigurer {
-
     @Override //api is not case sensitive
     public void configurePathMatch(PathMatchConfigurer configurer) {
         AntPathMatcher matcher = new AntPathMatcher();
         matcher.setCaseSensitive(false);
         configurer.setPathMatcher(matcher);
+    }
+
+    @Override //allows CORS from all origins
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
