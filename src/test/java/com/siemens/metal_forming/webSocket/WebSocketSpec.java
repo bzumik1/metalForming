e77853b471421003369c37bbf563f05953cc9c70
Jakub Znamenacek <jakub.znamenacek@siemens.com>
2021-02-12 09:04:23 +0100

    Fixes the bug in WebSocket test

--- src/test/java/com/siemens/metal_forming/webSocket/WebSocketSpec.java
+++ src/test/java/com/siemens/metal_forming/webSocket/WebSocketSpec.java
@@ -3,9 +3,11 @@ package com.siemens.metal_forming.webSocket;
 import com.siemens.metal_forming.connection.PlcData;
 import com.siemens.metal_forming.connection.opcua.PlcDataOpcua;
 import com.siemens.metal_forming.service.WebSocketService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -13,23 +15,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.StringMessageConverter;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.stomp.StompFrameHandler;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.messaging.simp.stomp.*;
+import org.springframework.util.StopWatch;
+import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 import java.lang.reflect.Type;
+import java.sql.SQLOutput;
 import java.util.List;
+import java.util.Timer;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.fail;
-
+@Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(MockitoExtension.class)
 public class WebSocketSpec {
@@ -47,39 +51,43 @@ public class WebSocketSpec {
     @BeforeEach
     public void initialize(){
         webSocketStompClient = new WebSocketStompClient(new SockJsClient(List.of(new WebSocketTransport(new StandardWebSocketClient()))));
+        webSocketStompClient.setMessageConverter(new StringMessageConverter());
     }
+
 
     @Test @DisplayName("test subscription to /topic/plcs/connection-status")
     void testSubscriptionToPlcConnectionEndpoint() throws Exception{
         CountDownLatch latch = new CountDownLatch(1);
 
-        webSocketStompClient.setMessageConverter(new MappingJackson2MessageConverter());
-
         StompSession stompSession = webSocketStompClient.connect("ws://localhost:" + port+"/api/ws", new StompSessionHandlerAdapter() {
+            @Override
+            public void afterConnected(StompSession session, StompHeaders headers) {
+                log.info("Client connected: headers {}", headers);
+                StompSession.Subscription subscription = session.subscribe("/topic/test", this);
+            }
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                latch.countDown();
+                log.info("Client received: payload {}, headers {}", payload, headers);
+            }
+            @Override
+            public void handleException(StompSession session, StompCommand command,
+                                        StompHeaders headers, byte[] payload, Throwable exception) {
+                log.error("Client error: exception {}, command {}, payload {}, headers {}",
+                        exception.getMessage(), command, payload, headers);
+            }
+            @Override
+            public void handleTransportError(StompSession session, Throwable exception) {
+                log.error("Client transport error: error {}", exception.getMessage());
+            }
         }).get(1, TimeUnit.SECONDS);
 
-        stompSession.subscribe("/topic/plcs/connection-status", new StompFrameHandler() {
-            @Override
-            public Type getPayloadType(StompHeaders stompHeaders) {
-                return String.class;
-            }
 
-            @Override
-            public void handleFrame(StompHeaders stompHeaders, Object o) {
-                latch.countDown();
-                System.out.println("RECEIVED: "+o);
-            }
-        });
+        Thread.sleep(100);
+        log.info("Server sending message: TEST");
+        simpMessagingTemplate.convertAndSend("/topic/test", "TEST");
 
-        //webSocketService.onConnectionStatusChange(plcData);
-        for(int i=0;i<5000;i++){
-            Thread.sleep(1000);
-            stompSession.send("/topic/plcs/connection-status", "SENT FROM SESSION!!!");
-            simpMessagingTemplate.convertAndSend("/topic/plcs/connection-status", "SENT FROM SIMPLE MESSAGING TEMPLATE!!!");
-        }
-
-
-        if (!latch.await(5000, TimeUnit.SECONDS)){
+        if (!latch.await(2, TimeUnit.SECONDS)){
             fail("Message not received");
         }
     }
