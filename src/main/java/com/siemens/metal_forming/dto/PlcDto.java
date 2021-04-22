c85381a0a8cd5073312a6e9242cd81e811b2511e
Jakub Znamenacek <znamenacekjakub@gmail.com>
2021-04-22 20:07:24 +0200

    Updates the structure of tool

--- src/main/java/com/siemens/metal_forming/dto/PlcDto.java
+++ src/main/java/com/siemens/metal_forming/dto/PlcDto.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.siemens.metal_forming.annotations.ValidIpAddress;
 import com.siemens.metal_forming.enumerated.ConnectionStatus;
 import lombok.Builder;
+import lombok.ToString;
 import lombok.Value;
 
 import javax.validation.constraints.NotBlank;
@@ -11,7 +12,7 @@ import javax.validation.constraints.NotBlank;
 public enum PlcDto {;
 
     public enum Request{;
-        @Value @Builder(toBuilder = true)
+        @Value @Builder(toBuilder = true) @ToString
         public static class Create{
             @ValidIpAddress
             String ipAddress;
@@ -20,7 +21,7 @@ public enum PlcDto {;
             String name;
         }
 
-        @Value @Builder(toBuilder = true)
+        @Value @Builder(toBuilder = true)@ToString
         public static class Update{
             @ValidIpAddress
             String ipAddress;
@@ -31,7 +32,7 @@ public enum PlcDto {;
     }
 
     public enum Response{;
-        @Value @Builder(toBuilder = true)
+        @Value @Builder(toBuilder = true) @ToString
         public static class Overview{
             Long id;
             String name;
@@ -40,19 +41,19 @@ public enum PlcDto {;
             ConnectionDto connection;
         }
 
-        @Value @Builder @JsonDeserialize(builder = Connection.ConnectionBuilder.class)
+        @Value @Builder @JsonDeserialize(builder = Connection.ConnectionBuilder.class) @ToString
         public static class Connection{
             Long id;
             ConnectionStatus connectionStatus;
         }
 
-        @Value @Builder
+        @Value @Builder @ToString
         public static class CurrentTool{
             Long id;
             Integer toolNumber;
         }
 
-        @Value @Builder
+        @Value @Builder @ToString
         public static class NewTool{
             Long id;
             ToolDto.Response.Overview newTool;
